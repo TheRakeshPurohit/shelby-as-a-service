83775b743ee9563509324fd61063d04d2c521200
shelbyjenkins <shelbyjenkins@gmail.com>
2023-07-09 21:46:25 +0000

    Final commit before adding ingest feature

--- app/discord_sprite.py
+++ app/discord_sprite.py
@@ -2,20 +2,18 @@
 import os
 import random
 import traceback
-
 import discord
 from discord.ext import commands
-
 from agents.logger_agent import LoggerAgent
 from agents.async_shelby_agent import ShelbyAgent
+from configuration.shelby_agent_config import AppConfig
 
 # For local use. When deployed as container there is no .env and enviroment vars are loaded from github secrets at deploy
 from dotenv import load_dotenv
 load_dotenv()
-bot_token = os.getenv('DISCORD_TOKEN')
-channel_id = int(os.environ['DISCORD_CHANNEL_ID'])
 
 log_agent = LoggerAgent('discord_sprite', 'discord_sprite.log', level='INFO')
+agent_config = AppConfig() 
 
 intents = discord.Intents.default()
 intents.guilds = True
@@ -26,25 +24,25 @@ bot = commands.Bot(command_prefix=commands.when_mentioned_or("!"), intents=inten
 # This prevents the bot from being added to servers that aren't approved
 @bot.event
 async def on_guild_join(guild):
-    if not any(channel.id == channel_id for channel in guild.channels):
+    if not any(channel.id == int(os.getenv('DISCORD_CHANNEL_ID')) for channel in guild.channels):
             log_agent.print_and_log(f'Leaving guild {guild.name} (ID: {guild.id}) due to missing channel.')
             await guild.leave()
-    channel = bot.get_channel(channel_id)
-    await channel.send(format_message(welcome_message, get_random_animal()))
+    channel = bot.get_channel(int(os.getenv('DISCORD_CHANNEL_ID')))
+    await channel.send(format_message(agent_config.discord_welcome_message, get_random_animal()))
             
 # App start up actions
 @bot.event
 async def on_ready():
     for guild in bot.guilds:
-        if not any(channel.id == channel_id for channel in guild.channels):
+        if not any(channel.id == int(os.getenv('DISCORD_CHANNEL_ID')) for channel in guild.channels):
             log_agent.print_and_log(f'Leaving guild {guild.name} (ID: {guild.id}) due to missing channel.')
             await guild.leave()
         
     log_agent.print_and_log(f'Bot has logged in as {bot.user.name} (ID: {bot.user.id})')
     log_agent.print_and_log('------')
-    channel = bot.get_channel(channel_id)
+    channel = bot.get_channel(int(os.getenv('DISCORD_CHANNEL_ID')))
 
-    await channel.send(format_message(welcome_message, get_random_animal()))
+    await channel.send(format_message(agent_config.discord_welcome_message, get_random_animal()))
 
 # On messages in the server. The bot should be configured in discord developer portal to only recieve messages where it's tagged,
 # but in the case it's configured to recieve all messages we cover for this case as well
@@ -59,20 +57,20 @@ async def on_message(message):
             await message.channel.send(f'No, I will not tell you about the rabbits, <@{message.author.id}>,.')
             return
         # Must be in the approved channel
-        if message.channel.id != channel_id:
+        if message.channel.id != int(os.getenv('DISCORD_CHANNEL_ID')):
             return
         
         request = message.content.replace(f'<@{bot.user.id}>', '').strip()
     
         # If question is too short
         if len(request.split()) < 4:
-            await message.channel.send(format_message(short_message, message.author.id))
+            await message.channel.send(format_message(agent_config.discord_short_message, message.author.id))
             return
         
         # Create thread
         thread = await message.create_thread(name=f"{get_random_animal()} {message.author.name}'s request", auto_archive_duration=60)
 
-        await thread.send(message_start)
+        await thread.send(agent_config.discord_message_start)
         
         try:
             request_response = await agent.run_request(request)
@@ -83,12 +81,16 @@ async def on_message(message):
             return 
         
         # Parse for discord and then respond
-        parsed_reponse = parse_discord_markdown(request_response)
-        log_agent.print_and_log(f'Parsed output: {parsed_reponse})')
-        await thread.send(parsed_reponse)
-
-        await thread.send(format_message(message_end, request_response['llm']))
-
+        
+        if isinstance(request_response, dict) and 'answer_text' in request_response:
+            parsed_reponse = parse_discord_markdown(request_response)
+            await thread.send(parsed_reponse)
+            await thread.send(format_message(agent_config.discord_message_end, request_response['llm']))
+            log_agent.print_and_log(f'Parsed output: {parsed_reponse})')
+        else:
+            await thread.send(request_response)
+            log_agent.print_and_log(f'Response output: {request_response})')
+        
 def parse_discord_markdown(request_response):
     # Start with the answer text
     markdown_string = f"{request_response['answer_text']}\n\n"
@@ -117,15 +119,11 @@ def format_message(template, var=None):
         return template.format(var)
     return template.format
 
-welcome_message = 'ima tell you about the {}.'
-short_message = '<@{}>, brevity is the soul of wit, but not of good queries. Please provide more details in your request.'
-message_start = 'Running request... relax, chill, and vibe a minute.'
-message_end = 'Generated by: {}\nMemory not enabled. Has no knowledge of past or current queries.\nFor code see https://github.com/ShelbyJenkins/shelby-as-a-service.'
 
 
 if __name__ == "__main__":
     agent = ShelbyAgent()
     # Runs the bot through the asyncio.run() function built into the library
-    bot.run(bot_token)
+    bot.run(os.getenv('DISCORD_TOKEN'))
 
 
